4319a0aea449355ffc14177430e0f2c56f513950
饺子w <ef@eternalflame.info>
2019-03-29 09:36:22 +0800

    don't exit when current user is unknown (#14)

--- config/locations.go
+++ config/locations.go
@@ -4,25 +4,17 @@ import (
 	"os/user"
 	"path/filepath"
 	"runtime"
-
-	"github.com/gotify/cli/utils"
 )
 
-func userDir() string {
-	usr, err := user.Current()
-	if err != nil {
-		utils.Exit1With(err)
-	}
-	return usr.HomeDir
-}
+func GetLocations() (res []string) {
+	res = append(res, "./cli.json")
 
-func GetLocations() []string {
-	inUserDir := filepath.Join(userDir(), ".gotify", "cli.json")
-	etcPath := "/etc/gotify/cli.json"
-	relativePath := "./cli.json"
-	if runtime.GOOS == "windows" {
-		return []string{relativePath, inUserDir}
-	} else {
-		return []string{relativePath, inUserDir, etcPath}
+	if usr, err := user.Current(); err != nil {
+		res = append(res, filepath.Join(usr.HomeDir, ".gotify", "cli.json"))
 	}
+
+	if runtime.GOOS != "windows" {
+		res = append(res, "/etc/gotify/cli.json")
+	}
+	return
 }
