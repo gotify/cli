684a19fe3276d6988594126825226b7261cd55e3
DCsunset <DCsunset@protonmail.com>
2024-07-30 20:33:51 -0400

    Add environment variable to trust custom certificate

--- utils/createhttpclient.go
+++ utils/createhttpclient.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"net/http"
 	"os"
 	"strings"
@@ -9,7 +10,27 @@ import (
 
 func CreateHTTPClient() *http.Client {
 	skipVerify := strings.ToLower(os.Getenv("GOTIFY_SKIP_VERIFY_TLS")) == "true"
+	certFile := os.Getenv("SSL_CERT_FILE")
+	if skipVerify && certFile != "" {
+		Exit1With("GOTIFY_SKIP_VERIFY_TLS and SSL_CERT_FILE shouldn't be set at the same time")
+	}
+
 	customTransport := http.DefaultTransport.(*http.Transport).Clone()
-	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: skipVerify}
+	rootCAs := customTransport.TLSClientConfig.RootCAs
+	if certFile != "" {
+		cert, err := os.ReadFile(certFile)
+		if err != nil {
+			Exit1With("Failed to read cert:", err)
+		}
+		rootCAs = x509.NewCertPool()
+		ok := rootCAs.AppendCertsFromPEM(cert)
+		if !ok {
+			Exit1With("Failed to parse cert", certFile)
+		}
+	}
+	customTransport.TLSClientConfig = &tls.Config{
+		InsecureSkipVerify: skipVerify,
+		RootCAs: rootCAs,
+	}
 	return &http.Client{Transport: customTransport}
 }
