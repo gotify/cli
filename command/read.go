3b4f0d89200d67a673c6173d0fa756aed80fce1e
eternal-flame-AD <yume@yumechi.jp>
2024-08-05 15:20:16 -0500

    Detect if STDIN is terminal and print hint
    Signed-off-by: eternal-flame-AD <yume@yumechi.jp>

--- command/read.go
+++ command/read.go
@@ -3,23 +3,37 @@ package command
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"io"
+	"os"
+	"runtime"
 	"strings"
 
 	"github.com/gotify/cli/v2/utils"
+	"github.com/mattn/go-isatty"
 )
 
 func readMessage(args []string, r io.Reader, output chan<- string, splitOnNull bool) {
 	defer close(output)
 
-	switch {
-	case len(args) > 0:
+	if len(args) > 0 {
 		if utils.ProbeStdin(r) {
 			utils.Exit1With("message is set via arguments and stdin, use only one of them")
 		}
 
 		output <- strings.Join(args, " ")
-	case splitOnNull:
+		return
+	}
+
+	if isatty.IsTerminal(os.Stdin.Fd()) {
+		eofKey := "Ctrl+D"
+		if runtime.GOOS == "windows" {
+			eofKey = "Ctrl+Z"
+		}
+		fmt.Fprintf(os.Stderr, "Enter your message, press Enter and then %s to finish:\n", eofKey)
+	}
+
+	if splitOnNull {
 		read := bufio.NewReader(r)
 		for {
 			s, err := read.ReadString('\x00')
@@ -37,7 +51,7 @@ func readMessage(args []string, r io.Reader, output chan<- string, splitOnNull b
 				}
 			}
 		}
-	default:
+	} else {
 		bytes, err := io.ReadAll(r)
 		if err != nil {
 			utils.Exit1With("cannot read", err)
